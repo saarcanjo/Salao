a80d284b5405008377eba0140e38db004f3b9cfa
aluno <aluno@labinfo>
2022-04-27 21:29:48 -0300

    Primeiro commit

---
+++ https:/github.com/saarcanjo/Salao.git/Salao/src/test/java/br/com/ifce/Salao/SalaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifce.Salao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
